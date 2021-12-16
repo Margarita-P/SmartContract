c36d4f91944da114792cf7d691088af63bc5641a
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2021-12-16 23:49:53 +0200

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,44 +1,57 @@
 pragma solidity 0.5.1;
 
-contract Counter {
-    uint public BuyerPays = 0;
-    uint public SellerSells = 0;
-    uint public CourierShips = 0;
-    uint public BuyerReceives = 0;
-    uint public ContractComplete = 0;
+contract CoinFlip {
+    mapping(address => uint) balances;
+    address payable[] recipients;
 
-    event Pay(uint value);
-    event Sell(uint value);
-    event Ship(uint value);
-    event bReceive(uint value);
-    event complete(uint value);
+    uint public outcome = 1;
+    uint private chosen;
+    uint public winner;
 
+    address payable player;
     
-    function Buyer() public {
-        BuyerPays = 1;
-        emit Pay(BuyerPays);
+    function flip() public{
+        uint temp = uint(keccak256(abi.encodePacked(now, block.difficulty, msg.sender))) % 5;
+        if(temp == 0 || temp == 1) {
+            outcome = 1;
+        }
+        else if (temp == 2 || temp == 3) {
+            outcome = 2;
+        }
+        else if (temp == 4 ) {
+            outcome = 3;
+        }
+    }
+    
+    function choose(uint number) public {
+        chosen = number;
     }
 
-    function Seller() public {
-        SellerSells = 1;
-        emit Sell(SellerSells);
+    function seeOutcome() public {
+        if(chosen == outcome){
+            winner = 1;
+        }
+        else {
+            winner = 2;
+        }
     }
 
-    function Courier() public {
-        CourierShips = 1;
-        emit Ship(CourierShips);
+    function SeeWinner(address payable addressX) public {
+        addressX.transfer(address(this).balance);
     }
 
-    function Receive() public {
-        BuyerReceives = 1;
-        emit bReceive(BuyerReceives);
+    function Loser(address payable addressX) public {
+        addressX.transfer(address(this).balance);
     }
 
-    function Complete() public {
-        if(BuyerPays == 1 && SellerSells == 1 && CourierShips == 1 && BuyerReceives == 1) {
-            ContractComplete = 1;
-        } 
-        emit complete(ContractComplete);
+    function invest() external payable {
+        if(msg.value < 1) {
+            revert();
+        }
+        balances[msg.sender] += msg.value;
     }
-   
+
+    function balanceOf() external view returns(uint) {
+        return address(this).balance;
+    }
 }
